cf75c6e3f3dadaa6320eda2136c691b66bbc63a2
Carl Jeske <carljeske@googlemail.com>
2020-07-18 12:42:03 +0100

    Only add new log handler when needed

--- mvrite/__init__.py
+++ mvrite/__init__.py
@@ -17,10 +17,13 @@ def get_logger(name):
     file_name = '{name}_{date}.log'.format(name=name, date=datetime.date.today())
     file_path = os.path.join(dir_name, file_name)
 
-    log_file_handler = logging.FileHandler(file_path, 'a')
-    log_file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-
     logger = logging.getLogger(name)
-    logger.addHandler(log_file_handler)
+
+    # Add new handler if necessary
+    if not logger.handlers:
+        log_file_handler = logging.FileHandler(file_path, 'a')
+        log_file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+        logger.addHandler(log_file_handler)
 
     return logger
